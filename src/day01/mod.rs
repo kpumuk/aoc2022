a4d71ba65413a560db5d7f35a9be755b5e81b57d
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2023-07-13 20:47:04 -0400

    Renamed function day1 to solution

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,4 +1,4 @@
-fn day1(input: &str, days: usize) -> u64 {
+fn solution(input: &str, days: usize) -> u64 {
     let mut elves = vec![0];
     for calories in input.lines().map(|v| v.parse().unwrap_or(0)) {
         match calories {
@@ -18,32 +18,32 @@ mod tests {
 
     #[test]
     fn part1_single_elve() {
-        assert_eq!(day1("5", 1), 5);
+        assert_eq!(solution("5", 1), 5);
     }
 
     #[test]
     fn part1_first_elve_wins() {
-        assert_eq!(day1("5\n\n4", 1), 5);
+        assert_eq!(solution("5\n\n4", 1), 5);
     }
 
     #[test]
     fn part1_example() {
-        assert_eq!(day1(EXAMPLE, 1), 24000);
+        assert_eq!(solution(EXAMPLE, 1), 24000);
     }
 
     #[test]
     fn part1_question() {
-        assert_eq!(day1(INPUT, 1), 75501);
+        assert_eq!(solution(INPUT, 1), 75501);
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(day1(EXAMPLE, 3), 45000);
+        assert_eq!(solution(EXAMPLE, 3), 45000);
     }
 
     #[test]
     fn part2_question() {
-        assert_eq!(day1(INPUT, 3), 215594);
+        assert_eq!(solution(INPUT, 3), 215594);
     }
 
     const EXAMPLE: &str = include_str!("example.txt");
